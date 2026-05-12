ec7fea90c877085a12d0f958a32dd7e4fcc570aa
zeviberlin <zeviberlin@gmail.com>
2026-05-12 11:53:06 -0400

    required + getters + TYPE_INFO macro

--- src/Option.hpp
+++ src/Option.hpp
@@ -9,6 +9,9 @@
 #ifndef C3PO_OPTION_HPP
 #define C3PO_OPTION_HPP
 
+#include <cxxabi.h>
+#include <typeinfo>
+#include <type_traits>
 #include <string>
 #include <memory>
 
@@ -23,34 +26,34 @@ namespace C3PO {
 /// will actually be held (via shared_ptr)
 /// inside of the parser
 template <typename T>
-class Option : OptionBase {
+class Option : public OptionBase {
 	private:
 		bool has_default = true;
 		std::shared_ptr<T> parsed = std::make_shared<T>();
 
 	public:
-		Option(const std::string& flag, const std::string& full, const std::string& desc, bool takes_value) :
-			OptionBase(flag, full, desc, takes_value) {}
+		Option(const std::string& flag, const std::string& full, const std::string& desc, bool takes_value, bool required) :
+			OptionBase(flag, full, desc, takes_value, required) {}
 
-		~Option() const override = default;
+		~Option() override = default;
 
 		/**/
 
 		/// Parse the arg into the given
 		/// type based on a set of rules
 		/// defined in the docs
-		void parse(const std::string& arg) override {
+		bool parse(const std::string& arg) override {
 			T result{};
-			parse_argument(arg, &result);
+			if (!parse_argument(arg, &result)) return false;
 			this->parsed = std::make_shared<T>(result);
 
 			this->has_default = false;
+			this->fill();
+			
+			return true;
 		}
-
-		// no-op, old model is deprecated
-		void parse() const override {}
-
-		bool has_default() const override {
+		
+		bool hasDefault() const override {
 			return this->has_default;
 		}
 
@@ -59,6 +62,37 @@ class Option : OptionBase {
 		const T& get() const {
 			return *(this->parsed);
 		}
+
+		constexpr T getT() const { return T{}; }
 };
+
+
+
+// helper to detect if a type is a specialization of a template
+template <typename T, template <typename...> class Template>
+struct is_specialization_of : std::false_type {};
+
+template <template <typename...> class Template, typename... Args>
+struct is_specialization_of<Template<Args...>, Template> : std::true_type {};
+
+/**/
+
+template <typename T>
+std::string TYPE_INFO() {
+	if constexpr (std::is_same<T, std::string>::value) {
+		return "string";
+	} else if constexpr (is_specialization_of<T, std::vector>::value) {
+		using Inner = typename T::value_type;
+    return "list<" + TYPE_INFO<Inner>() + ">";
+	} else {
+		int status;
+  	char* demangled = abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, &status);
+  	std::string result = (status == 0) ? demangled : typeid(T).name();
+  	free(demangled);
+  	return result;
+  }
+}
 	
 }
+
+#endif /* C3PO_OPTION_HPP */
